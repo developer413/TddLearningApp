a3a1f417c09bdfb400009dd30c67462980c56bcc
srujanNeom <srujan.reddy@publicissapient.com>
2023-01-23 23:21:23 +0530

    s-2-10 test implementation

--- app/src/test/java/com/p413/tddlearning/EngineTest.kt
+++ app/src/test/java/com/p413/tddlearning/EngineTest.kt
@@ -14,4 +14,12 @@ class EngineTest {
         assertEquals(true, engine.isTurnedOn)
         assertEquals(95, engine.temperature)
     }
+
+    @Test
+    fun turnOff() {
+        engine.turnOff()
+
+        assertEquals(false, engine.isTurnedOn)
+        assertEquals(15, engine.temperature)
+    }
 }
